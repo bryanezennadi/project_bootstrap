b5196b8ff6272b0a301fb4443e38e73f535b3185
bryan ezennadi <121224272+bryanezennadi@users.noreply.github.com>
2024-12-19 18:13:29 +0100

    consegna finale

---
+++ project_bootstrap/altri_files/functions.js
@@ -0,0 +1,4 @@
+document.getElementById("bottoneTrasporto").addEventListener("click", function() {
+    // Cambia la finestra attuale alla nuova pagina HTML
+    window.location.href = "transportLayer.html";
+});
