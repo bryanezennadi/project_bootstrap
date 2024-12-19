77d00c1762c12fadd0d4d6f6858c02cb9fe1a0ed
bryan ezennadi <121224272+bryanezennadi@users.noreply.github.com>
2024-12-19 18:14:12 +0100

    Delete altri_files directory

--- altri_files/functions.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("bottoneTrasporto").addEventListener("click", function() {
-    // Cambia la finestra attuale alla nuova pagina HTML
-    window.location.href = "transportLayer.html";
-});
